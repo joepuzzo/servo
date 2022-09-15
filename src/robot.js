d5f1107a91c478d217d546c84ed7a2a57f987762
joepuzzo <jgpuzzo2@gmail.com>
2022-09-15 11:44:07 -0400

    added limit adj to j2

--- src/robot.js
+++ src/robot.js
@@ -62,7 +62,7 @@ export class Robot extends EventEmitter   {
 
       // Create Motors
       this.motors.j0 = new Motor({ stepper: 0, id: 'j0', board, stepPin: 0, dirPin: 1, limitPin: 26, enablePin: 33, encoderPinA: 14, encoderPinB: 15, limPos: 170, limNeg: 170, stepDeg: 44.44444444, limitAdj: 2, maxSpeed: 1000 });
-      this.motors.j1 = new Motor({ stepper: 1, id: 'j1', board, stepPin: 2, dirPin: 3, limitPin: 27, enablePin: 34, encoderPinA: 17, encoderPinB: 16, limPos: 42, limNeg: 90, stepDeg: 55.55555556, maxSpeed: 1000 });
+      this.motors.j1 = new Motor({ stepper: 1, id: 'j1', board, stepPin: 2, dirPin: 3, limitPin: 27, enablePin: 34, encoderPinA: 17, encoderPinB: 16, limPos: 42, limNeg: 90, stepDeg: 55.55555556, maxSpeed: 1000, limitAdj: -1.5 });
       this.motors.j2 = new Motor({ stepper: 2, id: 'j2', board, stepPin: 4, dirPin: 5, limitPin: 28, enablePin: 35, encoderPinA: 19, encoderPinB: 18, limPos: 20, limNeg: 145, stepDeg: 55.55555556, limitDir: BACKWARDS, limitAdj: 2 });
       this.motors.j3 = new Motor({ stepper: 3, id: 'j3', board, stepPin: 6, dirPin: 7, limitPin: 29, enablePin: 36, encoderPinA: 20, encoderPinB: 21, limPos: 165, limNeg: 165, stepDeg: 42.72664356, invertEnable: false, limitDir: BACKWARDS });
       this.motors.j4 = new Motor({ stepper: 4, id: 'j4', board, stepPin: 8, dirPin: 9, limitPin: 30, enablePin: 37, encoderPinA: 23, encoderPinB: 22, limPos: 100, limNeg: 100, stepDeg: 21.86024888, limitAdj: -2 });
